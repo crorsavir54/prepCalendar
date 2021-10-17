896c882201b9d7342255dcf79f8dd831dbe890f8
Carlos Rivas <coriv@pm.me>
2021-10-17 19:28:55 +0800

    Initial Commit

---
+++ prep/prepApp.swift
@@ -0,0 +1,17 @@
+//
+//  prepApp.swift
+//  prep
+//
+//  Created by coriv🧑🏻‍💻 on 10/17/21.
+//
+
+import SwiftUI
+
+@main
+struct prepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
