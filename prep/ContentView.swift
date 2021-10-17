31e5c448b57be77a4d5d912742eb6677c2bc9eb9
Carlos Rivas <coriv@pm.me>
2021-10-18 06:35:59 +0800

    SwiftUI Inset

--- prep/ContentView.swift
+++ prep/ContentView.swift
@@ -9,8 +9,62 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        
+        List {
+            
+            
+//            VStack(alignment: .leading) {
+//                HStack {
+//                    Image(systemName: "wifi.square")
+//                        .resizable()
+//                        .frame(width: 26, height: 26)
+//                    Text("WLAN")
+//
+//                }
+//                Divider()
+//            }
+            VStack(alignment: .leading) {
+                HStack {
+                    Image(systemName: "wifi.square.fill")
+                        .resizable()
+                        .frame(width: 26, height: 26)
+                        .foregroundStyle(.white, .blue)
+                    VStack(alignment: .leading, spacing: 0) {
+                        Spacer()
+                        Text("WI-FI")
+                            .padding(.bottom,10)
+                        Divider()
+                    }
+                }
+                HStack {
+                    ZStack {
+                        Image(systemName: "square.fill")
+                            .resizable()
+                            .foregroundColor(.green)
+                        Image(systemName: "antenna.radiowaves.left.and.right")
+                            .foregroundColor(.white)
+                            
+                        
+                    }
+                    .frame(width: 26, height: 26)
+                    VStack(alignment: .leading, spacing: 0) {
+                        Spacer()
+                        Text("Cellular")
+                            .padding(.bottom,10)
+                        
+                        Divider()
+                            
+                    }
+                }
+            }
+            .listRowSeparator(.hidden)
+        }
+        
+        .listStyle(.insetGrouped)
+        
+        
+        
     }
 }
 
